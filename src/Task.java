7c437b8457c2b94043b3d4711665eb1372967f03
safiulinrm <zloyangel666999@yandex.ru>
2022-02-25 19:06:52 +0300

    fix : не смог сделать enum, во втором спринте его нет, только в третьем в середине появляется

--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,13 @@
 public class Task {
-    String taskName;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
     int number = 0;
     String status = "NEW";
 }
