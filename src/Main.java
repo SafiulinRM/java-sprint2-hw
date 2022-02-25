7c5b03680ed6e30382da21f852e524a006983f23
safiulinrm <zloyangel666999@yandex.ru>
2022-02-25 19:02:18 +0300

    fix : подкоректировал названия методов

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         Manager manager = new Manager();
-        Epic buyCar = new Epic("Покупка машины");
-        Subtask saveMoney = new Subtask("Накопить денег", buyCar.number);
-        Subtask goToCarDealership = new Subtask("Пойти в автосасалон", buyCar.number);
+        Epic buyCar = new Epic();
+        buyCar.setName("Покупка Машины");
+        Subtask saveMoney = new Subtask(buyCar.number);
+        saveMoney.setName("Накопить денег");
+        Subtask goToCarDealership = new Subtask(buyCar.number);
+        goToCarDealership.setName("Пойти в автосалон");
         manager.createEpic(buyCar);
         manager.createSubtask(saveMoney, buyCar.number);
         manager.createSubtask(goToCarDealership, buyCar.number);
-        Epic moving = new Epic("Переезд");
-        Subtask packBackpack = new Subtask("Собрать рюкзак", moving.number);
+        Epic moving = new Epic();
+        moving.setName("Переезд");
+        Subtask packBackpack = new Subtask(moving.number);
+        packBackpack.setName("Собрать рюкзак");
         manager.createEpic(moving);
         manager.createSubtask(packBackpack, moving.number);
         manager.getAll();
